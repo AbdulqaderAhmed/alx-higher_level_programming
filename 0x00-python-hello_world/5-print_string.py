18e4534f317ed986c92373a0bce6d537a594a658
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-05-31 21:15:14 +0300

    Printing the string "Holberton School" three times and then printing the first 9 characters

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -3,5 +3,4 @@
 # Printing the string "Holberton School" three times and then printing the first 9 characters of the
 # string.
 str = "Holberton School"
-print(str * 3)
-print(str[:9])
+print("{}{}{}\n{}".format(str, str, str, str[0:9]))
