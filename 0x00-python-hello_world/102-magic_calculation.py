8705dd45a8a5c0f456400f2788629f11ba4181ae
AbdulqaderAhmed <abdulqaderahmed32@gmail.com>
2022-05-31 21:37:21 +0300

    new

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    """
-    It adds 98 to the result of a to the power of b
-    
-    :param a: the first integer
-    :param b: 98
-    :return: 98 + (a ** b)
-    """
     return (
         98 + (a ** b))
